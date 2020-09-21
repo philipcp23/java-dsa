900972f141b29c8615267e63527ab9485cf725c0
Phil Paolo <philipcpaolo@gmail.com>
2020-09-21 11:21:01 -0400

    git commit
    git commit
    	deleted:    getting-started-programming-java/09/demos/exercise-files/after/CalcEngine/src/com/pluralsight/calcengine/Main.java

--- getting-started-programming-java/02/demos/exercise-files/after/Organized/src/com/pluralsight/organized/Main.java
+++
@@ -1,8 +0,0 @@
-package com.pluralsight.organized;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    System.out.println("We got organized!");
-    }
-}
--- getting-started-programming-java/03/demos/exercise-files/ReadMe.txt
+++
@@ -1,6 +0,0 @@
-For more details about Floating Point types:
-http://bit.ly/psjavafp
-
-For more details about the behavior of type conversions:
-http://bit.ly/pstypeconversion
-
