afbf52e71fd924d81704995a29d37c913fac46c7
Phil Paolo <philipcpaolo@gmail.com>
2020-10-01 14:33:40 -0400

    java folders

---
+++ Arrays, Lists, Autoboxing & Unboxing/GroceryList/src/GroceryList.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class GroceryList {
+    private ArrayList<String> groceryList = new ArrayList<String>();
+
+    public void addGroceryItem(String item) {
+        groceryList.add(item);
+    }
+
+    public void printGroceryList() {
+        System.out.println("You have + " + groceryList.size() + " items in your list.");
+        for (int i = 0; i < groceryList.size(); i++) {
+            System.out.println("Item " + (i + 1) + ": " + groceryList.get(i);
+        }
+    }
+
+    public void modifyGroceryItem(int position, String newItem) {
+        groceryList.set(position, newItem);
+        System.out.println("Grocery list item modified at position " + (position + 1));
+    }
+
+    public void removeGroceryItem(int position) {
+        String item = groceryList.get(position);
+        groceryList.remove(position);
+        System.out.println("Grocery list item: " +  item + " removed at position " + (position + 1));
+    }
+}
