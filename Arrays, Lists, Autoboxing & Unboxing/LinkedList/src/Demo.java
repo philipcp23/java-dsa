e2f66e651cb1e57e960a979f2aa38738ebb62fe2
Phil Paolo <philipcpaolo@gmail.com>
2020-10-13 20:20:18 -0400

    java folders

---
+++ Arrays, Lists, Autoboxing & Unboxing/LinkedList/src/Demo.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+
+public class Demo {
+    public static void main(String[] args) {
+        LinkedList<String> vacation = new LinkedList<String>();
+        vacation.add("Italy");
+        vacation.add("Spain");
+        vacation.add("Ireland");
+        vacation.add("Germany");
+        vacation.add("France");
+        vacation.add("Hawaii");
+        vacation.add("Switzerland");
+        vacation.add("Australia");
+
+        printList(vacation);
+    }
+
+    private static void printList(LinkedList<String> vacation) {
+    }
+}
--- Arrays, Lists, Autoboxing & Unboxing/LinkedList/src/Main.java
+++ Arrays, Lists, Autoboxing & Unboxing/LinkedList/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Customer customer = new Customer("Phil", 60.75);
@@ -5,5 +7,21 @@ public class Main {
         anotherCustomer = customer;
         anotherCustomer.setValue(12.50);
         System.out.println("Customer: " + customer.getName() + "'s balance: " + customer.getValue());
+
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+
+        intList.add(1);
+        intList.add(3);
+        intList.add(4);
+        for (int i = 0; i < intList.size(); i++) {
+            System.out.println(i + " : " + intList.get(i));
+        }
+
+        //intList.add(0, 0);
+        intList.add(1, 2);
+
+        for (int i = 0; i < intList.size(); i++) {
+            System.out.println(i + " : " + intList.get(i));
+        }
     }
 }
