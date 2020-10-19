84e866634c8e17f90ff8301a62edd4359b4dc591
Phil Paolo <philipcpaolo@gmail.com>
2020-10-19 16:14:52 -0400

    java folders

--- Arrays, Lists, Autoboxing & Unboxing/LinkedList/src/Demo.java
+++ Arrays, Lists, Autoboxing & Unboxing/LinkedList/src/Demo.java
@@ -1,6 +1,7 @@
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
@@ -47,12 +48,54 @@ public class Demo {
         }
 
         listIterator.add(newVacation);
+        return true;
     }
 
+    private static void visit(LinkedList cities) {
+        var scanner = new Scanner(System.in);
+        boolean quit = false;
+        ListIterator listIterator = cities.listIterator();
 
+        if (cities.getFirst() == "") {
+            System.out.println("No cities in the schedule");
+        } else {
+            System.out.println("Now visiting" + listIterator.next());
+            printMenu();
+        }
 
+        while (!quit) {
+            int action = scanner.nextInt();
+            scanner.nextLine();
+            switch (action) {
+                case 0:
+                    System.out.println("Vacation has come to an end");
+                    quit = true;
+                    break;
+                case 1:
+                    if (listIterator.hasNext()) {
+                        System.out.println("Now visiting " + listIterator.next());
+                    } else {
+                        System.out.println("End of the list .");
+                    }
+                    break;
+                case 2:
+                    if (listIterator.hasPrevious()) {
+                        System.out.println("Now visiting " + listIterator.hasPrevious());
+                    } else {
+                        System.out.println("We are at the start of the list");
+                    }
+                    break;
+            }
+        }
+    }
 
-
+    private static void printMenu() {
+        System.out.println("Available actions: \npress ");
+        System.out.println("0 - to quit\n" +
+                "1 - go to the next city\n" +
+                "2 - go to previous city\n" +
+                "3 - print menu options");
+    }
 
 
 }
