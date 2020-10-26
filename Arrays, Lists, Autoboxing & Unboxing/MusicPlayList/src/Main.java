74fe20517de8b739c91f3057e38f63466ee3dffa
Phil Paolo <philipcpaolo@gmail.com>
2020-10-26 18:39:00 -0400

    java folders

--- Arrays, Lists, Autoboxing & Unboxing/MusicPlayList/src/Main.java
+++ Arrays, Lists, Autoboxing & Unboxing/MusicPlayList/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         /*
         Create a program that implements a playlist for songs
         Create a Song class having Title and Duration for a song.
-        The program will have an album class containing a list of songs.
+        The program will have an Album class containing a list of songs.
         The albums will be stored in ArrayList
         Songs from different albums can be added to the playlist and will appear in the list in order
         they are added.
