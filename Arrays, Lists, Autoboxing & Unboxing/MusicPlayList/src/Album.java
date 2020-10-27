9f392feb1596ba6bea6d1a5c0e8ee22d516be10e
Phil Paolo <philipcpaolo@gmail.com>
2020-10-27 07:45:27 -0400

    java folders

--- Arrays, Lists, Autoboxing & Unboxing/MusicPlayList/src/Album.java
+++ Arrays, Lists, Autoboxing & Unboxing/MusicPlayList/src/Album.java
@@ -2,11 +2,30 @@ import java.util.ArrayList;
 
 public class Album {
     private String album;
-    private Song song;
+    private String artist;
+    private ArrayList<Song> songs;
 
-    public Album(String album, Song song) {
+
+    public Album(String album, String artist, ArrayList<Song> songs) {
         this.album = album;
-        this.song = song;
+        this.artist = artist;
+        this.songs = new ArrayList<Song>();
+    }
+
+    private boolean addSong(String title, double duration) {
+        if (findSong(title) == null) {
+            songs.add(new Song(title, duration));
+            return true;
+        }
+        return false;
+    }
+
+    private Song findSong(String title) {
+        for (Song checkedSong: songs) {
+            if (checkedSong.getTitle().equals(title)) {
+                return checkedSong;
+            }
+        }
     }
 
 
