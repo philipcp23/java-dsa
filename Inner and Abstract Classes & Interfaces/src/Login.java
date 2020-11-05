36b770fbb0499c50b4258510cfd540592337cf1b
Phil Paolo <philipcpaolo@gmail.com>
2020-11-05 14:58:18 -0500

    java folders

---
+++ Inner and Abstract Classes & Interfaces/src/Login.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Login implements Saveable {
+    @Override
+    public List<ArrayList> saveValues() {
+        return null;
+    }
+
+    @Override
+    public void populateObjects() {
+
+    }
+}
--- Inner and Abstract Classes & Interfaces/src/Saveable.java
+++ Inner and Abstract Classes & Interfaces/src/Saveable.java
@@ -1,2 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public interface Saveable {
+
+    List<ArrayList> saveValues();
+    void populateObjects();
 }
